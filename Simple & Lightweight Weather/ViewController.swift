6503c7bf7665d254c832289404d2b26fd60d9cfd
Roman Radchuk <roman.radchuk@globallogic.com>
2022-08-29 13:50:40 +0300

    Initial Commit

---
+++ Simple & Lightweight Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple & Lightweight Weather
+//
+//  Created by Roman Radchuk on 29.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
